d6776b083ec8d3be3e63bebe261f1f80550aa68a
Cosmina <cosmina.marc@yahoo.com>
2022-08-03 13:01:05 +0300

    TetTest

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestTest {
-    public static void main(String[] args) {
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://madisonperfumery.com/?lang=ro");
-driver.quit();
+    public class MadisonTest {
+
+        public static void main (String [] args) {
+            ChromeDriver driver = new ChromeDriver();
+            driver.get("https://madisonperfumery.com/?lang=ro");
+ driver.quit();
+        }
     }
 }
